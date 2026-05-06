c2106dcec206c81b242911c392b2d2fb1a075915
らぐち <corekaraweb@gmail.com>
2026-05-06 11:30:15 +0900

    最初のコミット

---
+++ src/chap5_12/Main.java
@@ -0,0 +1,23 @@
+package chap5_12;
+
+class A {
+	String val ="A";
+	void print() {
+		System.out.print(val);
+	}
+}
+
+class B extends A {
+	String val = "Bあ";
+}
+
+public class Main {
+	public static void main(String[] args) {
+		A a = new A();
+		A b = new B();
+		System.out.print(a.val);
+		System.out.print(b.val);
+		a.print();
+		b.print();
+	}
+}
