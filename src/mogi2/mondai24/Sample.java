df55ce586f3d24e7dbff5ab258aaeef89ddf3057
らぐち <corekaraweb@gmail.com>
2026-06-07 23:11:26 +0900

    模擬試験②の問題24のソースコード追加

---
+++ src/mogi2/mondai24/Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println(test("A"));
+	}
+	public static String test(Object obj) {
+		String s = "";
+		if (!(obj instanceof String str)) {
+			s = obj.toString();
+		} else {
+			s = str.toLowerCase();
+		}
+		return s;
+	}
+}	
