1e2fa14225d00a8fbef302518c3bfa1a7d7149cd
らぐち <corekaraweb@gmail.com>
2026-06-07 23:17:54 +0900

    模擬試験2の問題24についてコメントを追加

--- src/mogi2/mondai24/Sample.java
+++ src/mogi2/mondai24/Sample.java
@@ -3,12 +3,14 @@ public class Sample {
 		System.out.println(test("A"));
 	}
 	public static String test(Object obj) {
-		String s = "";　
+		String s = "";
 		if (!(obj instanceof String str)) {
 			s = obj.toString();
 		} else { 
 			s = str.toLowerCase(); 
 		}
 		return s;
+		// 黒本の解説では「コンパイルエラー」が表示されると記載されているが、実際にコードを実行すると
+		// elseブロック内の str.toLowerCase()が実行され、小文字の「a」が表示される。
 	}
 }	
